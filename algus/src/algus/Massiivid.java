82a50e95aff4ab5a308f1e4b10cabfcc6a5655f0
if17 <if17@A406-1.INFORMAATIKA>
2018-02-01 11:15:50 +0200

    min/max

--- algus/src/algus/Massiivid.java
+++ algus/src/algus/Massiivid.java
@@ -1,8 +1,48 @@
 package algus;
 
+
 import java.util.Arrays;
 
 public class Massiivid {
+    //Koosta funktsioon, mis tagastab kaheelemendilise massiivi
+    //Mille esimeseks elemendiks on etteantud arvudest suurim teiseks, vähim
+    
+    public static int[] highsetLowest(int[] array){
+        int[] result = new int[2];
+        int highest = array[0];
+        int lowest = array[0];
+        
+        if(array ==null){
+            throw new RuntimeException("parameetri väärtuseks on null");
+        }
+        if(array.length==0){
+            throw new RuntimeException("Prameetriks on tühi massiiv");
+        }
+        
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > highest){
+                highest = array[i];
+            }
+        }
+        result[1] = highest;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] < lowest){
+                lowest = array[i];
+            }
+        }
+        result[0] = lowest;
+        return result;
+    }
+    
+    public static int summa(int[] arvud){
+        int summa =0;
+        for(int i =0; i<arvud.length; i++){
+            summa = summa + arvud[i];
+        }
+        return summa;
+    }
+    
+    
     public static void main(String[] arg){
         int[] pikkused={176,159,364,214,121};
         System.out.println("Kokku: " + pikkused.length);
@@ -24,7 +64,10 @@ public class Massiivid {
         sum = 0;
         for (int pikkus : pikkused){sum+=pikkus;}
         System.out.println(sum);
+        System.out.println(summa(pikkused));
         
         System.out.println(Arrays.stream(pikkused).sum());
+        System.out.println("Väikseim arv: " + highsetLowest(pikkused)[0]);
+        System.out.println("Suurim arv: " + highsetLowest(pikkused)[1]);
     }
 }
